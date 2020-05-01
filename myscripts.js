f6d76a67ae1b8fcb2311694d1712393258121bd5
Deepak260797 <deepak.kumrawat@okcredit.in>
2020-05-01 21:52:56 +0530

    last commits

--- myscripts.js
+++ myscripts.js
@@ -6,33 +6,17 @@
       });
     }
 
-
     async function testWebShare() {
-
-      // if (navigator.share === undefined) {
-      //   // logError('Error: Unsupported feature: navigator.share()');
-      //   return;
-      // }
 
       const title_input = document.querySelector('#title');
       const text_input = document.querySelector('#text');
       const url_input = document.querySelector('#url');
       const file_input = document.querySelector('#files');
 
-
       const title = title_input.value;
       const text = text_input.value;
       const url = url_input.value;
       const files = file_input.files;
-
-
-      // if (files && files.length > 0) {
-      //   if (!navigator.canShare || !navigator.canShare({files})) {
-      //     // logError('Error: Unsupported feature: navigator.canShare()');
-      //     return;
-      //   }
-      // }
-
 
       try {
         await navigator.share({files, title, text, url});
@@ -43,11 +27,10 @@
       }
     }
 
-    async function testWebShareDelay() {
-      await sleep(6000);
-      testWebShare();
-    }
-
+    // async function testWebShareDelay() {
+    //   await sleep(6000);
+    //   testWebShare();
+    // }
 
     function onLoad() {
       document.querySelector('#share').addEventListener('click', testWebShare);

7f3a3ad5367faf5f07e98af6152af6f6ea3b6957
Deepak260797 <deepak.kumrawat@okcredit.in>
2020-05-01 23:18:39 +0530

    last commits

--- myscripts.js
+++ myscripts.js
@@ -1,20 +1,20 @@
    
     function testWebShare() {
 
-      // const title_input = document.querySelector('#title');
-      // const text_input = document.querySelector('#text');
-      // const url_input = document.querySelector('#url');
+      const title_input = document.querySelector('#title');
+      const text_input = document.querySelector('#text');
+      const url_input = document.querySelector('#url');
       const file_input = document.querySelector('#files');
 
-      // const title = title_input.value;
-      // const text = text_input.value;
-      // const url = url_input.value;
+      const title = title_input.value;
+      const text = text_input.value;
+      const url = url_input.value;
       const files = file_input.files;
 
-      const title = "Hello";
-      const text = "Hii";
-      const url = "https://example.com";
-      const files = files;
+      // const title = "Hello";
+      // const text = "Hii";
+      // const url = "https://example.com";
+      // const files = files;
 
       try {
         await navigator.share({files, title, text, url});

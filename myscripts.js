2eb0524c1a642f8cf351a014cc4b9cc61f14822a
Deepak260797 <deepak.kumrawat@okcredit.in>
2020-05-01 22:36:21 +0530

    last commits

--- myscripts.js
+++ myscripts.js
@@ -1,21 +1,20 @@
     'use strict';
 
-    // function sleep(delay) {
-    //   return new Promise(resolve => {
-    //     setTimeout(resolve, delay);
-    //   });
-    // }
-
     async function testWebShare() {
 
-      const title_input = document.querySelector('#title');
-      const text_input = document.querySelector('#text');
-      const url_input = document.querySelector('#url');
-      const file_input = document.querySelector('#files');
+      // const title_input = document.querySelector('#title');
+      // const text_input = document.querySelector('#text');
+      // const url_input = document.querySelector('#url');
+      // const file_input = document.querySelector('#files');
 
-      const title = title_input.value;
-      const text = text_input.value;
-      const url = url_input.value;
+      // const title = title_input.value;
+      // const text = text_input.value;
+      // const url = url_input.value;
+      // const files = file_input.files;
+
+      const title = "Hello";
+      const text = "Hii";
+      const url = "https://example.com";
       const files = file_input.files;
 
       try {
@@ -26,11 +25,6 @@
         console.log(error);
       }
     }
-
-    // async function testWebShareDelay() {
-    //   await sleep(6000);
-    //   testWebShare();
-    // }
 
     function onLoad() {
       document.querySelector('#share').addEventListener('click', testWebShare);

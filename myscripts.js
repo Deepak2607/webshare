792423af609d869fee5102a8d3c3f8abda501d76
Deepak260797 <deepak.kumrawat@okcredit.in>
2020-05-01 21:47:02 +0530

    last commits

--- myscripts.js
+++ myscripts.js
@@ -6,29 +6,13 @@
       });
     }
 
-    // function logText(message, isError) {
-    //   if (isError)
-    //     console.error(message);
-    //   else
-    //     console.log(message);
-
-    //   const p = document.createElement('p');
-    //   if (isError)
-    //     p.setAttribute('class', 'error');
-    //   document.querySelector('#output').appendChild(p);
-    //   p.appendChild(document.createTextNode(message));
-    // }
-
-    // function logError(message) {
-    //   logText(message, true);
-    // }
-
 
     async function testWebShare() {
-      if (navigator.share === undefined) {
-        // logError('Error: Unsupported feature: navigator.share()');
-        return;
-      }
+
+      // if (navigator.share === undefined) {
+      //   // logError('Error: Unsupported feature: navigator.share()');
+      //   return;
+      // }
 
       const title_input = document.querySelector('#title');
       const text_input = document.querySelector('#text');
@@ -42,23 +26,21 @@
       const files = file_input.files;
 
 
-      if (files && files.length > 0) {
-        if (!navigator.canShare || !navigator.canShare({files})) {
-          // logError('Error: Unsupported feature: navigator.canShare()');
-          return;
-        }
-      }
+      // if (files && files.length > 0) {
+      //   if (!navigator.canShare || !navigator.canShare({files})) {
+      //     // logError('Error: Unsupported feature: navigator.canShare()');
+      //     return;
+      //   }
+      // }
 
 
       try {
         await navigator.share({files, title, text, url});
-        // logText('Successfully sent share');
+        console.log('Successfully shared');
       } 
       catch (error) {
         console.log(error);
-        // logError('Error sharing: ' + error);
       }
-
     }
 
     async function testWebShareDelay() {
@@ -68,17 +50,11 @@
 
 
     function onLoad() {
-
       document.querySelector('#share').addEventListener('click', testWebShare);
-
       if (navigator.share === undefined) {
         if (window.location.protocol === 'http:') {
           window.location.replace(window.location.href.replace(/^http:/, 'https:'));
         } 
-        // else {
-        //   logError('Error: You need to use a browser that supports this draft ' +
-        //            'proposal.');
-        // }
       }
     }
 

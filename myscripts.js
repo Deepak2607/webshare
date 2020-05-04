bc3d63b6035cc6634a80760d27aca0b6aa721b96
Deepak2607 <deepakkumrawat8@gmail.com>
2020-05-04 15:08:21 +0530

    ---

--- myscripts.js
+++ myscripts.js
@@ -4,16 +4,24 @@
       // const title_input = document.querySelector('#title');
       // const text_input = document.querySelector('#text');
       // const url_input = document.querySelector('#url');
-      const file_input = document.querySelector('#files');
+      // const file_input = document.querySelector('#files');
 
       // const title = title_input.value;
       // const text = text_input.value;
       // const url = url_input.value;
-      const files = file_input.files;
+      // const files = file_input.files;
+
+      const image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Rick_Astley_Dallas.jpg/267px-Rick_Astley_Dallas.jpg";
+      const response = await fetch(image_url);
+      const blob = await response.blob();
+      const file = new File([blob], 'rick.jpg', {type: blob.type});
+      console.log(file);
+
 
       const title = "Share";
       const text = "Share the application";
       const url = "https://okcredit.in";
+      const files = [file];
 
       try {
         await navigator.share({files, title, text, url});
